2ac3a9bcd55cf8448725ca42b13de2deacb90b79
Ronald Pedra <69393789+ronaldpedra@users.noreply.github.com>
2025-10-10 17:58:16 -0300

    Update config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# ./congig.py
+
+import os
+from dotenv import load_dotenv
+
+
+# Encontra o caminho absoluto para o diretório raiz do projeto
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config:
+    """Configurações base da aplicação."""
+    # Chave secreta para proteger sessões e cookies.
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    # Configuração do SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
