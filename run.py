759b716bd00655975ae784e4e152255e85656d05
Ronald Pedra <69393789+ronaldpedra@users.noreply.github.com>
2025-10-10 18:42:48 -0300

    Update run.py

---
+++ run.py
@@ -0,0 +1,25 @@
+# /projeto-ferias/run.py
+
+from app import create_app, db
+from app.models import Usuario, Secao, PeriodoAquisitivo, SolicitacaoFerias
+
+
+# Cria a instância da aplicação usando a nossa fábrica
+app = create_app()
+
+@app.shell_context_processor
+def make_shell_context():
+    """
+    Disponibiliza os modelos no shell do Flask para facilitar testes.
+    Comando: flask shell
+    """
+    return {
+        'db': db,
+        'Usuario': Usuario,
+        'Secao': Secao,
+        'PeriodoAquisitivo': PeriodoAquisitivo,
+        'SolicitacaoFerias': SolicitacaoFerias
+    }
+
+if __name__ == '__main__':
+    app.run(debug=True)
