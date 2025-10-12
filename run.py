240a9bbc316cf12bdb06b723dce7126d6132037c
Ronald Pedra <69393789+ronaldpedra@users.noreply.github.com>
2025-10-12 00:44:23 -0300

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,7 @@
 
 from app import create_app, db
 from app.models import Usuario, Secao, PeriodoAquisitivo, SolicitacaoFerias
+import click
 
 # Cria instância da aplicação usando a nossa fábrica
 app = create_app()
@@ -21,6 +22,28 @@ def make_shell_context():
         'PeriodoAquisitivo': PeriodoAquisitivo,
         'SolicitacaoFerias': SolicitacaoFerias
     }
+
+@app.cli.command('create-gestor')
+@click.argument('nome_guerra')
+@click.argument('identidade')
+@click.argument('password')
+def create_gestor(nome_guerra, identidade, password):
+    """Cria um novo usuário com o papel de Gestor"""
+    if Usuario.query.filter_by(identidade=identidade).first():
+        print(f'Erro: Usuário com identidade {identidade} já existe.')
+        return
+    
+    gestor = Usuario(
+        nome_completo = f'{nome_guerra} (Gestor)',
+        nome_guerra = nome_guerra,
+        identidade = identidade,
+        posto_grad = 'Admin'
+        papel = PapelUsuario.GESTOR
+    )
+    gestor.set_password(password)
+    db.session.add(gestor)
+    db.session.commit()
+    print(f"Usuário Gestor '{nome_guerra}' criado com sucesso!")
 
 if __name__ == '__main__':
     app.run(debug=True)
