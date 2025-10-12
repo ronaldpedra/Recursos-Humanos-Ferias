8ab4c0c7cd0e835b828b7a57d6b025f5767b8927
Ronald Pedra <69393789+ronaldpedra@users.noreply.github.com>
2025-10-12 01:07:55 -0300

    Update run.py

--- run.py
+++ run.py
@@ -4,10 +4,11 @@
 
 import click
 from app import create_app, db
-from app.models import Usuario, Secao, PeriodoAquisitivo, SolicitacaoFerias
+from app.models import Usuario, Secao, PeriodoAquisitivo, SolicitacaoFerias, PapelUsuario
 
 # Cria instância da aplicação usando a nossa fábrica
 app = create_app()
+
 
 @app.shell_context_processor
 def make_shell_context():
@@ -23,27 +24,29 @@ def make_shell_context():
         'SolicitacaoFerias': SolicitacaoFerias
     }
 
-@app.cli.command('create-gestor')
-@click.argument('nome_guerra')
-@click.argument('identidade')
-@click.argument('password')
+
+@app.cli.command("create-gestor")
+@click.argument("nome_guerra")
+@click.argument("identidade")
+@click.argument("password")
 def create_gestor(nome_guerra, identidade, password):
     """Cria um novo usuário com o papel de Gestor"""
     if Usuario.query.filter_by(identidade=identidade).first():
-        print(f'Erro: Usuário com identidade {identidade} já existe.')
+        print(f"Erro: Usuário com identidade {identidade} já existe.")
         return
-    
+
     gestor = Usuario(
-        nome_completo = f'{nome_guerra} (Gestor)',
-        nome_guerra = nome_guerra,
-        identidade = identidade,
-        posto_grad = 'Admin'
-        papel = PapelUsuario.GESTOR
+        nome_completo=f"{nome_guerra} (Gestor)",
+        nome_guerra=nome_guerra,
+        identidade=identidade,
+        posto_grad="Admin",
+        papel=PapelUsuario.GESTOR
     )
     gestor.set_password(password)
     db.session.add(gestor)
     db.session.commit()
     print(f"Usuário Gestor '{nome_guerra}' criado com sucesso!")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
